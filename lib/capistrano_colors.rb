52665789a7d3206beee1553e49ab4f6c3ca127eb
Mathias Stjernström <mathias@globalinn.com>
2008-12-23 00:13:09 +0100

    Moved console colors to constants and some cleanup

--- lib/capistrano_colors.rb
+++ lib/capistrano_colors.rb
@@ -3,36 +3,38 @@ puts "\e[36m    == capistrano_colors loaded ==\e[0m"
 module Capistrano
   class Logger
 
+    CC_STD    = "0" 
+    CC_RED    = "31"
+    CC_GREEN  = "32"
+    CC_YELLOW = "33"
+    CC_BLUE   = "34"
+
     def colorize(message, color, nl = "\n")
       "\e[#{color}m" + message.strip + "\e[0m#{nl}"
     end
 
     def debug(message, line_prefix=nil)
       if message =~ /executing `.*/
-        # log(DEBUG, colorize("==[task]====================================", "32"), line_prefix)
-        log(DEBUG, colorize("== Currently " + message, "32"), line_prefix)
-        # log(DEBUG, colorize("============================================", "32"), line_prefix)
+        log(DEBUG, colorize("== Currently " + message, CC_GREEN), line_prefix)
       else          
-        log(DEBUG, colorize(message, "33"), line_prefix)            
+        log(DEBUG, colorize(message, CC_YELLOW), line_prefix)            
       end
     end
 
     def info(message, line_prefix=nil)
-      if message =~ /.*out\] (fatal:|ERROR:).*/
-        color = "31"
-      elsif message =~ /Permission denied/
-        color = "31"
+      if message =~ /.*out\] (fatal:|ERROR:).*/ || message =~ /Permission denied/
+        color = CC_RED
       else
-        color = "0"
+        color = CC_STD
       end
       log(INFO, colorize(message,color), line_prefix)        
     end
     
     def important(message, line_prefix=nil)
       if line_prefix =~ /^err ::/
-        color = "31"
+        color = CC_RED
       else
-        color = 34
+        color = CC_BLUE
       end
       log(IMPORTANT, colorize(message, color), line_prefix) if !message.strip.empty?
     end
